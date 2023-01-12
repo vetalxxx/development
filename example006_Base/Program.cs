5665a8a941639ed9ab25bfee3066003a41a67a75
Vitaly <vetalxxx2011@mail.ru>
2023-01-12 16:03:51 +0400

    kettlebells

---
+++ example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int a = new Random().Next(1,9);
+int b = new Random().Next(1,9);
+int c = new Random().Next(1,9);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Наш max = ");
+Console.WriteLine(max);
