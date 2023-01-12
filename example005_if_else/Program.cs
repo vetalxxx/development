45a9e5d0b899bc303fcb1f6e740a480fd62d7830
Vitaly <vetalxxx2011@mail.ru>
2023-01-12 15:36:56 +0400

    if else and letter case

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя ");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "маша")  // регистр букв убираем 
+{
+  Console.WriteLine("Ура, ты Маша");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(name);
+}
