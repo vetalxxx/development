fb788d71344343876adde7df2d9718249d3e8579
Vitaly <vetalxxx2011@mail.ru>
2023-01-16 13:44:29 +0400

    ex1 square

---
+++ example1/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); // меняет текст в цифры
+int result = number * number;
+Console.WriteLine("Квадрат этого числа: " + result);
