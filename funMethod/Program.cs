fa25fac409a4428628db7108fb804a57196ef6c8
Vitaly <vetalxxx2011@mail.ru>
2023-01-16 20:10:48 +0400

    funMethod

---
+++ funMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 8, 4, 5, 6, 7, 8, 9 };
+
+int n = array.Length;
+
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  } //завершит работу как только найдет число и не будет смотреть если там еще такие числа
+  index++;
+}
