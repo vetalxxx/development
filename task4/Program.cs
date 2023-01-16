9967c8733c64e82ce31f50c490191a963f9c894f
Vitaly <vetalxxx2011@mail.ru>
2023-01-16 17:23:32 +0400

    all even

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введи число > 3");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = 0;
+
+while (number1 > number2 + 1)
+{
+  number2 = number2 + 2;
+  Console.Write(number2 + " ");
+}
