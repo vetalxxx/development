997976edb25a69e02e448c6f42610dbe0b9a11d0
Vitaly <vetalxxx2011@mail.ru>
2023-01-16 16:30:46 +0400

    max of three

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Вводи первое число из трех");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Вводи второе число из трех");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Вводи третье число из трех");
+int number3 = int.Parse(Console.ReadLine());
+
+int number_max = number1;
+
+if (number2 >= number_max)
+{
+  number_max = number2;
+}
+if (number3 >= number_max)
+{
+  number_max = number3;
+}
+Console.WriteLine(" Число " + number_max + " наибольшее из трех введеных тобой чисел");
+
